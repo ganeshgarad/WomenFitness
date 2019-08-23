52987a3b38d5c1d103d68f8ba3115ac69399ec3b
najamiqbal <najamiqbal@mte.edu.pk>
2019-08-23 14:55:25 +0500

    ok

--- app/src/main/java/com/sardar/softsolstudio/femalehomeworkout/fragments/HomeFragment.java
+++ app/src/main/java/com/sardar/softsolstudio/femalehomeworkout/fragments/HomeFragment.java
@@ -15,7 +15,7 @@ import com.sardar.softsolstudio.femalehomeworkout.R;
 
 public class HomeFragment extends Fragment implements View.OnClickListener{
     View view;
-    CardView abs_beginner,abs_inter,abs_advance,arm_cardview;
+    CardView abs_beginner,abs_inter,abs_advance,arm_cardview,butt_beginner,butt_inter,butt_advance,chest_cardview,leg_beginner,leg_inter,leg_advance;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -29,10 +29,24 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
         abs_inter=view.findViewById(R.id.abs_inter_cardview);
         abs_advance=view.findViewById(R.id.abs_advance_cardview);
         arm_cardview=view.findViewById(R.id.arms_cardview);
+        butt_advance=view.findViewById(R.id.butt_advance_cardview);
+        butt_beginner=view.findViewById(R.id.butt_beginner_cardview);
+        butt_inter=view.findViewById(R.id.butt_inter_cardview);
+        chest_cardview=view.findViewById(R.id.chest_cardview);
+        leg_advance=view.findViewById(R.id.leg_advance_cardview);
+        leg_beginner=view.findViewById(R.id.leg_beginner_cardview);
+        leg_inter=view.findViewById(R.id.leg_inter_cardview);
         abs_beginner.setOnClickListener(this);
         abs_inter.setOnClickListener(this);
         abs_advance.setOnClickListener(this);
         arm_cardview.setOnClickListener(this);
+        butt_inter.setOnClickListener(this);
+        butt_beginner.setOnClickListener(this);
+        butt_advance.setOnClickListener(this);
+        chest_cardview.setOnClickListener(this);
+        leg_inter.setOnClickListener(this);
+        leg_beginner.setOnClickListener(this);
+        leg_advance.setOnClickListener(this);
     }
 
     @Override
@@ -45,45 +59,50 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.abs_beginner_cardview:
-                SingleWorkOutList fragment=new SingleWorkOutList();
-                Bundle args = new Bundle();
-                args.putString("lebal", "ABS beginner");
-                fragment.setArguments(args);
-                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.main_frame, fragment);
-                fragmentTransaction.addToBackStack("forgetpass_fragment");
-                fragmentTransaction.commit();
+                CallFragment("ABS beginner");
                 break;
             case R.id.abs_inter_cardview:
-                SingleWorkOutList fragment1=new SingleWorkOutList();
-                Bundle args1 = new Bundle();
-                args1.putString("lebal", "ABS intermediate");
-                fragment1.setArguments(args1);
-                FragmentTransaction fragmentTransaction1 = getFragmentManager().beginTransaction();
-                fragmentTransaction1.replace(R.id.main_frame, fragment1);
-                fragmentTransaction1.addToBackStack("forgetpass_fragment");
-                fragmentTransaction1.commit();
+                CallFragment("ABS intermediate");
                 break;
             case R.id.abs_advance_cardview:
-                SingleWorkOutList fragment2=new SingleWorkOutList();
-                Bundle args2 = new Bundle();
-                args2.putString("lebal", "ABS advanced");
-                fragment2.setArguments(args2);
-                FragmentTransaction fragmentTransaction2 = getFragmentManager().beginTransaction();
-                fragmentTransaction2.replace(R.id.main_frame, fragment2);
-                fragmentTransaction2.addToBackStack("forgetpass_fragment");
-                fragmentTransaction2.commit();
+                CallFragment("ABS advanced");
                 break;
             case R.id.arms_cardview:
-                SingleWorkOutList fragment3=new SingleWorkOutList();
-                Bundle args3 = new Bundle();
-                args3.putString("lebal", "ARM workout");
-                fragment3.setArguments(args3);
-                FragmentTransaction fragmentTransaction3 = getFragmentManager().beginTransaction();
-                fragmentTransaction3.replace(R.id.main_frame, fragment3);
-                fragmentTransaction3.addToBackStack("forgetpass_fragment");
-                fragmentTransaction3.commit();
+                CallFragment("ARM workout");
                 break;
+            case R.id.butt_beginner_cardview:
+                CallFragment("BUTT beginner");
+                break;
+            case R.id.butt_advance_cardview:
+                CallFragment("BUTT advanced");
+                break;
+            case R.id.butt_inter_cardview:
+                CallFragment("BUTT intermediate");
+                break;
+            case R.id.leg_beginner_cardview:
+                CallFragment("LEG beginner");
+                break;
+            case R.id.leg_inter_cardview:
+                CallFragment("LEG intermediate");
+                break;
+            case R.id.leg_advance_cardview:
+                CallFragment("LEG advanced");
+                break;
+            case R.id.chest_cardview:
+                CallFragment("CHEST workout");
+                break;
+
         }
+    }
+
+    private void CallFragment(String workout) {
+        SingleWorkOutList fragment3=new SingleWorkOutList();
+        Bundle args3 = new Bundle();
+        args3.putString("lebal", workout);
+        fragment3.setArguments(args3);
+        FragmentTransaction fragmentTransaction3 = getFragmentManager().beginTransaction();
+        fragmentTransaction3.replace(R.id.main_frame, fragment3);
+        fragmentTransaction3.addToBackStack("forgetpass_fragment");
+        fragmentTransaction3.commit();
     }
 }
